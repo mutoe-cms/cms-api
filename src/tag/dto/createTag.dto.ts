db44a42ed17f32baaccdcc163af6af44d042cc62
mutoe <mutoe@foxmail.com>
2021-03-28 22:27:14 +0800

    fix: fix dto optional decorator

--- src/tag/dto/createTag.dto.ts
+++ src/tag/dto/createTag.dto.ts
@@ -1,4 +1,4 @@
-import { ApiProperty } from '@nestjs/swagger'
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'
 import { IsNotEmpty } from 'class-validator'
 
 export class CreateTagDto {
@@ -10,6 +10,6 @@ export class CreateTagDto {
   @ApiProperty({ example: 'semantic-ui' })
   readonly key: string
 
-  @ApiProperty({ example: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>' })
+  @ApiPropertyOptional({ example: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>' })
   readonly description?: string
 }
