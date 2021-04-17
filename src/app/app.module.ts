a214628c405483d022fba37d137ea33ba7506d06
mutoe <mutoe@foxmail.com>
2021-04-17 21:22:09 +0800

    feat(env): add local env support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from 'src/app/app.controller'
 import { ArticleModule } from 'src/article/article.module'
@@ -9,6 +10,9 @@ import { UserModule } from 'src/user/user.module'
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.local', '.env'],
+    }),
     TypeOrmModule.forRoot(),
     UserModule,
     AuthModule,
